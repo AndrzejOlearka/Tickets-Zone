cd65162bdb8aa0e093ce1194419adff3b76bcd40
AndrzejOlearka <olearka1992@gmail.com>
2021-05-02 16:25:39 +0200

    ADD: DEVISE auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'home/index'
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
